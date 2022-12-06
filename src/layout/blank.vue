75c7f24e216bc662236c396af7f56a81debbe175
Bean Boonyawat <bean1888@outlook.com>
2022-12-07 00:14:51 +0700

    layout

---
+++ src/layout/blank.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app class="background">
+    <router-view />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'BlankLayout'
+}
+</script>
---
+++ src/layout/frontend.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app class="background">
+    <router-view></router-view>
+    <v-bottom-navigation v-model="value" color="primary" fixed>
+      <v-btn v-for="(item, index) in buttonObj" :key="index" :value="item.value" height="100%" color="white">
+        <span>{{ item.name }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'FrontendLayout',
+  data () {
+    return {
+      value: '',
+      buttonObj: [
+        {
+          value: 'home',
+          name: 'หน้าแรก',
+          icon: 'mdi-home-outline',
+          path: ''
+        },
+        {
+          value: 'food',
+          name: 'สั่งอาหาร',
+          icon: 'mdi-food-off-outline',
+          path: ''
+        },
+        {
+          value: 'cart',
+          name: 'ตะกร้า',
+          icon: 'mdi-cart-outline',
+          path: ''
+        },
+        {
+          value: 'order',
+          name: 'คำสั่งซื้อ',
+          icon: 'mdi-clock-time-five-outline',
+          path: ''
+        }
+      ]
+    }
+  }
+}
+</script>
