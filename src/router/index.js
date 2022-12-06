886c755c0e6c39c7ec7d680125bdcb0a1d5141f0
Bean Boonyawat <bean1888@outlook.com>
2022-12-07 00:15:59 +0700

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: () => import('@/layout/blank.vue'),
+    children: [
+      {
+        path: '',
+        name: 'StartPage',
+        component: () => import('@/views/Frontend/startPage.vue')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/foodtruck',
+    component: () => import('@/layout/frontend.vue'),
+    children: [
+      {
+        path: '',
+        name: 'HomePage',
+        component: () => import('@/views/Frontend/homePage.vue')
+      }
+    ]
   }
 ]
 
